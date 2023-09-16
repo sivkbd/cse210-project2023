d0e995baff25619db28fb7de71065a3594e0c04c
sivkbd <siviwe.kibido@gmail.com>
2023-09-16 12:58:33 +0200

    Completed five small C# programming assignments.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        //Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+
+        string userName = Name();
+        int userNumber = Number();
+
+        int squaredNumber = SquareNumber(userNumber);
+
+        Results(userName, squaredNumber);
+    }    
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the programm");
     }
+
+    static string Name()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+    static int Number()
+    {
+        Console.Write("Please enter your favorite number:");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void Results(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+
+
+    
 }
